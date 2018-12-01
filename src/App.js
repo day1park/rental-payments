6912abe9d0725ec904ea6f54f6431a79d62c7bf6
day1park <daypark2408@gmail.com>
2018-12-01 16:16:23 +1100

    fetch data from api and setstate with parsed json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import "./App.css";
 import Header from "./header";
 import Table from "./table";
 
+let API = "https://hiring-task-api.herokuapp.com/v1/leases/:id";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLease: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          currentLease: json
+        });
+      })
+      .catch(error => console.log("parsing failed"));
+  }
+
   render() {
     return (
       <div className="App">
